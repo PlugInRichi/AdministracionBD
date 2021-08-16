474f29315e61b70cbe362c6e7a02dcb888f5686b
seraf7 <zaske_4@hotmail.com>
2021-08-15 19:56:42 -0500

    Creación de sqlnet.ora y corrección al crear BD

--- crea-bd-ordinario.sql
+++ crea-bd-ordinario.sql
@@ -3,12 +3,19 @@
 -- @Descripcion: Crea la base de datos, hay que levantar la instancia como
 -- nomount previo a la ejecución de este script
 
-connect sys/system as sysdba
+prompt Conectando como usuario sys
+connect sys/hola1234* as sysdba
+
+--Se inicia la instancia en modo nomount
+startup nomount
+
 whenever sqlerror exit rollback;
+
+prompt Creando base de datos
 
 create database gacaproy
 user sys identified by system
-user sysbackup identified by system
+user system identified by system
 logfile group 1 (
 '/u01/app/oracle/oradata/GACAPROY/disk_3/redo01a.log',
 '/u01/app/oracle/oradata/GACAPROY/disk_4/redo01b.log') size 50m blocksize 512,
@@ -25,30 +32,39 @@ maxdatafiles 1024
 character set AL32UTF8
 national character set AL16UTF16
 extent management local
+  --tablespace para system
+  datafile '/u01/app/oracle/oradata/GACAPROY/disk_1/system01.dbf'
+    size 700m reuse autoextend on next 10240k maxsize unlimited
+  --tablespace auxiliar
+  sysaux datafile '/u01/app/oracle/oradata/GACAPROY/disk_1/sysaux01.dbf'
+    size 550m reuse autoextend on next 10240k maxsize unlimited
+  --tablespace default
+  default tablespace users
+    datafile '/u01/app/oracle/oradata/GACAPROY/disk_1/users01.dbf'
+    size 100m reuse autoextend on maxsize unlimited
+  --tablespace temporal
+  default temporary tablespace temptbs1
+    tempfile '/u01/app/oracle/oradata/GACAPROY/disk_1/temp01.dbf'
+    size 20m reuse autoextend on next 640k maxsize unlimited
+  --tablespace para datos undo
+  undo tablespace undotbs1
+    datafile '/u01/app/oracle/oradata/GACAPROY/disk_1/undotbs01.dbf'
+    size 200m reuse autoextend on next 5120k maxsize unlimited;
 
-datafile '/u01/app/oracle/oradata/GACAPROY/disk_1/system01.dbf'
-size 700m reuse autoextend on next 10240k maxsize unlimited
+prompt Actualizando contraseñas
+-- Actualiza contraseña de sys
+alter user sys identified by system;
+-- Actualiza contraseña de sysbackup
+alter user sysbackup identified by system;
 
-sysaux datafile '/u01/app/oracle/oradata/GACAPROY/disk_1/sysaux01.dbf'
-size 550m reuse autoextend on next 10240k maxsize unlimited
-
-default tablespace users
-datafile '/u01/app/oracle/oradata/GACAPROY/disk_1/users01.dbf'
-size 100m reuse autoextend on maxsize unlimited
-
-
-default temporary tablespace temptbs1
-tempfile '/u01/app/oracle/oradata/GACAPROY/disk_1/temp01.dbf'
-size 20m reuse autoextend on next 640k maxsize unlimited
-
-undo tablespace undotbs1
-datafile '/u01/app/oracle/oradata/GACAPROY/disk_1/undotbs01.dbf'
-size 200m reuse autoextend on next 5120k maxsize unlimited;
-
+prompt Creando wallet
 -- Creamos el Wallet
 alter system set encryption key identified by "wallet_password";
--- Abrimos el Wallet
-alter system set encryption wallet open identified by "wallet_password"
 
-alter user sys identified by system;
-alter user sysbackup identified by system;
+-- Se reinicia la BD
+-- shutdown immediate
+-- startup mount
+-- Abrimos el Wallet, requerido despues de cada reinicio
+-- alter system set encryption wallet open identified by "wallet_password";
+-- Para cerrar el wallet
+-- ALTER SYSTEM SET ENCRYPTION WALLET CLOSE IDENTIFIED BY "wallet_password";
--- crea-pwd-param-oracle.sh
+++ crea-pwd-param-oracle.sh
@@ -1,7 +1,7 @@
 # @Autor García García Ricardo - Humberto Serafín Castillo López
 # @Fecha 3/07/2021
 # @Descripcion: Creación del archivo de passwords y archivo de parámetros
-#               Ejecutar como usuario oracle 
+#               Ejecutar como usuario oracle
 
 #Se crea variable de entorno
 echo "Creando variable de entorno ORACLE_SID..."
@@ -9,11 +9,13 @@ export ORACLE_SID=gacaproy
 
 rutaArchivoPass=$ORACLE_HOME/dbs/orapwgacaproy
 rutaArchivoParam=$ORACLE_HOME/dbs/initgacaproy.ora
+rutaArchivoSqlnet=$ORACLE_HOME/network/admin/sqlnet.ora
 
 orapwd FILE=$rutaArchivoPass \
   force=y \
   FORMAT=12.2 \
   SYS=password \
+  SYSBACKUP=password
 
 echo "
 db_name=gacaproy
@@ -22,3 +24,11 @@ control_files = (/u01/app/oracle/oradata/GACAPROY/disk_1/control01.ctl,
                  /u01/app/oracle/oradata/GACAPROY/disk_4/control03.ctl)
 memory_target=768M
 " > $rutaArchivoParam
+
+echo "
+ENCRYPTION_WALLET_LOCATION=
+ (SOURCE=
+  (METHOD=file)
+   (METHOD_DATA=
+    (DIRECTORY=$ORACLE_BASE/oradata/GACAPROY/disk_1/wallets)))
+" > $rutaArchivoSqlnet
