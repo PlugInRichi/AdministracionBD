1fbdde1a012fc506db15832446296466d9acd728
seraf7 <zaske_4@hotmail.com>
2021-08-17 18:31:45 -0500

    Modos de conexión permanentes y uso de servidor compartido

--- config-modos-conexion.sql
+++ config-modos-conexion.sql
@@ -2,11 +2,14 @@
 --@Fecha creación: 15/07/2021
 --@Descripción: Configuracion y conexion en modo compartido
 
+-- Conexión como sys
+connect sys/system as sysdba
+
 -- Creación de 2 dispatchers con protocolo TCP
-alter system set dispatchers='(dispatchers=2)(protocol=tcp)';
+alter system set dispatchers='(dispatchers=2)(protocol=tcp)' scope=both;
 
 -- Creación de 4 shared servers
-alter system set shared_servers=5;
+alter system set shared_servers=5 scope=both;
 
 Prompt Dispatchers y Shared Servers configurados
 show parameter dispatchers
@@ -16,7 +19,7 @@ show parameter shared_servers
 alter system register;
 
 --Añade el nuevo archivo de tnsnames
-!cp tnsnames.ora /u01/app/oracle/product/19.3.0/dbhome_1/network/admin/tnsnames.ora
+--!cp tnsnames.ora /u01/app/oracle/product/19.3.0/dbhome_1/network/admin/tnsnames.ora
 
 -- Mostrar servicios registrados en el listener
 !lsnrctl services
